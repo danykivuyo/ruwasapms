72837b3c6081e6bc47879df1c9b7d27780bbd1e0
dany kivuyo <danykivuyo@gmail.com>
2024-06-19 06:55:53 +0300

    changes on customer card

--- app/Livewire/DashboardCustomersCard.php
+++ app/Livewire/DashboardCustomersCard.php
@@ -2,6 +2,9 @@
 
 namespace App\Livewire;
 
+use App\Models\Meter;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class DashboardCustomersCard extends Component
@@ -11,6 +14,8 @@ class DashboardCustomersCard extends Component
     public $active_customers;
     public $pd_customers = 32;
     public $hhc_customers = 19;
+    public $meters;
+    public $active_meters;
 
     public $pd_active_customers = 12;
     public $hhc_active_customers = 8;
@@ -28,6 +33,45 @@ class DashboardCustomersCard extends Component
 
     public function get_hhc_customers()
     {
+        $user = User::find(Auth::user()->id);
+        if ($user->role == "cbwso") {
+            $this->meters = Meter::all()
+                ->where('cbwso', $user->cbwso);
+            $this->active_meters = Meter::all()
+                ->where('status', 1)
+                ->where('cbwso', $user->cbwso);
+        } else if ($user->role == "district") {
+            $this->meters = Meter::all()
+                ->where('district', $user->district);
+            $this->active_meters = Meter::all()
+                ->where('status', 1)
+                ->where('district', $user->district);
+        } else if ($user->role == "region") {
+            $this->meters = Meter::all()
+                ->where('region', $user->region);
+            $this->active_meters = Meter::all()
+                ->where('status', 1)
+                ->where('region', $user->region);
+        } else if ($user->role == "country") {
+            $this->meters = Meter::all();
+            $this->active_meters = Meter::all()
+                ->where('status', 1);
+        } else if ($user->role == "admin") {
+            $this->meters = Meter::all();
+            $this->active_meters = Meter::all()
+                ->where('status', 1);
+        } else {
+            $this->meters = 0;
+            $this->active_meters = 0;
+            return;
+        }
+
+        $this->hhc_customers = $this->meters
+            ->where('type', "HHC")
+            ->count();
+        $this->hhc_active_customers = $this->active_meters
+            ->where('type', "HHC")
+            ->count();
         $this->message = "HHC Customers";
         $this->customers = $this->hhc_customers;
         $this->active_customers = $this->hhc_active_customers;
@@ -38,6 +82,50 @@ class DashboardCustomersCard extends Component
         $this->message = "PD Customers";
         $this->customers = $this->pd_customers;
         $this->active_customers = $this->pd_active_customers;
+    }
+
+    public function mount()
+    {
+        $user = User::find(Auth::user()->id);
+        if ($user->role == "cbwso") {
+            $this->meters = Meter::all()
+                ->where('cbwso', $user->cbwso);
+            $this->active_meters = Meter::all()
+                ->where('status', 1)
+                ->where('cbwso', $user->cbwso);
+        } else if ($user->role == "district") {
+            $this->meters = Meter::all()
+                ->where('district', $user->district);
+            $this->active_meters = Meter::all()
+                ->where('status', 1)
+                ->where('district', $user->district);
+        } else if ($user->role == "region") {
+            $this->meters = Meter::all()
+                ->where('region', $user->region);
+            $this->active_meters = Meter::all()
+                ->where('status', 1)
+                ->where('region', $user->region);
+        } else if ($user->role == "country") {
+            $this->meters = Meter::all();
+            $this->active_meters = Meter::all()
+                ->where('status', 1);
+        } else if ($user->role == "admin") {
+            $this->meters = Meter::all();
+            $this->active_meters = Meter::all()
+                ->where('status', 1);
+        } else {
+            $this->meters = 0;
+            $this->active_meters = 0;
+            return;
+        }
+
+        $this->hhc_customers = $this->meters
+            ->where('type', "HHC")
+            ->count();
+        $this->hhc_active_customers = $this->active_meters
+            ->where('type', "HHC")
+            ->count();
+
     }
     public function render()
     {
