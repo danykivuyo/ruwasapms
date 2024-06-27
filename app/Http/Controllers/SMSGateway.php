03df04404b280b8510f7f03cff3558a1a502cb5c
dany kivuyo <danykivuyo@gmail.com>
2024-06-27 05:52:57 +0300

    sms gateway intergration

---
+++ app/Http/Controllers/SMSGateway.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cbwso;
+use App\Models\Customer;
+use App\Models\Meter;
+use Illuminate\Http\Request;
+
+class SMSGateway extends Controller
+{
+    //
+    private $meter_number;
+    private $balance;
+    private $meter_id;
+    public function meter(Request $request)
+    {
+        $this->meter_number = substr($request->from_number, -9);
+        // $this->meter_number = "0" + $this->meter_number;
+        $meter = Meter::where('meter_number', $this->meter_number)->first();
+        // return $this->meter_number;
+        preg_match('/#(\d+\.\d+)\r\n#0$/', $request->sms, $matches);
+
+        if (!empty($matches)) {
+            $cbwso = Cbwso::where('name', $meter->cbwso)->first();
+            $customer = Customer::where('meter_id', $meter->meter_id)->first();
+            $value = $matches[1];
+            $meter->balance = $value;
+            $customer->balance = number_format($value * $cbwso->tarrif, 2);
+            $customer->save();
+            $meter->save();
+
+            return [
+                'success' => true,
+                'message' => "Successfully updated"
+            ];
+        } else {
+            return [
+                'success' => false,
+                'message' => "Empty"
+            ];
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\MeterLogController;
+use App\Http\Controllers\SMSGateway;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('meter-logs', MeterLogController::class);
+
+Route::post('/sms-gateway', [SMSGateway::class, 'meter']);
