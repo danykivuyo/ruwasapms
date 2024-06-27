1cad3ca0cd8204a4a38ed0bd83f6baeb75c303fe
dany kivuyo <danykivuyo@gmail.com>
2024-06-27 11:25:10 +0300

    test gateway

--- app/Http/Controllers/SMSGateway.php
+++ app/Http/Controllers/SMSGateway.php
@@ -22,7 +22,7 @@ class SMSGateway extends Controller
         // return $meter->cbwso;
         // preg_match('/#(\d+\.\d+)\r\n#0$/', $request->sms, $matches);
         preg_match('/#(\d+\.\d{2})\\\\r\\\\n/', $request->sms, $matches);
-        // return $matches;
+        return $matches;
         return "$request->sms";
         if (!empty($matches)) {
             $cbwso = Cbwso::where('name', $meter->cbwso)->first();
